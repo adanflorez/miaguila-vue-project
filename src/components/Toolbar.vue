ee9ee29d39cf933e652435a247c241a3dc9b3adb
Adan Florez <adan.florez@peiky.com>
2020-12-11 11:05:52 -0500

    Dynamic name and cursor-pointer class

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -3,13 +3,13 @@
     <div class="d-flex justify-content-between align-items-center h-100 p-3">
       <h4>{{ path }}</h4>
       <div class="d-flex align-items-center">
-        <i class="fas fa-search mr-3"></i>
-        <i class="fas fa-bell mr-3"></i>
-        <p class="text-sm mr-3 font-weight-600">Jones Ferdinand</p>
+        <i class="fas fa-search mr-3 cursor-pointer"></i>
+        <i class="fas fa-bell mr-3 cursor-pointer"></i>
+        <p class="text-sm mr-3 font-weight-600">{{ userEmail }}</p>
         <div
           class="pic-profile d-flex justify-content-center align-items-center"
         >
-          <i class="fas fa-user"></i>
+          <i class="fas fa-user cursor-pointer"></i>
         </div>
       </div>
     </div>
@@ -25,6 +25,10 @@ export default {
       let path = this.$route.path.split('/')[1];
       let capitalized = path.charAt(0).toUpperCase() + path.slice(1);
       return capitalized;
+    },
+    userEmail() {
+      let user = JSON.parse(localStorage.getItem('userInfo'));
+      return user.email;
     }
   }
 };
