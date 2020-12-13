c65394f6425588df54b3d1506998d17b32b65527
Adan Florez <adan.florez@peiky.com>
2020-12-13 14:19:27 -0500

    Multiples environments

--- src/http/axios/index.js
+++ src/http/axios/index.js
@@ -1,8 +1,9 @@
 // axios
 import axios from 'axios';
+const API = process.env.VUE_APP_API;
 
 export default {
   usersList() {
-    return axios.get('https://api.mocki.io/v1/853804f2/users');
+    return axios.get(API);
   },
 };
